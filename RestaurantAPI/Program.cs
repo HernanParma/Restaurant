65bf16052c8aaeff2545f0fe0ea3db0c2b736ed1
HernanParma <hernanparma22@gmail.com>
2025-09-11 22:48:23 -0300

    ultimos detalles y ok para entrega

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -9,45 +9,44 @@ using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddSwaggerGen(c => { c.EnableAnnotations(); });
-builder.Services.AddControllers();
 builder.Services.AddControllers()
     .AddJsonOptions(o =>
     {
         o.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
     });
-builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "RestaurantAPI",
-        Version = "v1"
-    });
+    c.EnableAnnotations();
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestaurantAPI", Version = "v1" });
+
+    // no mostrar pricesort en schemas
     c.MapType<PriceSort>(() => new OpenApiSchema
     {
         Type = "string",
-        Enum = new List<IOpenApiAny>
-        {
-            new OpenApiString("asc"),
-            new OpenApiString("desc")
-        }
+        Enum = new List<IOpenApiAny> { new OpenApiString("asc"), new OpenApiString("desc") }
     });
 });
 
-// custom (Inyecciones de dependencias)
+// custom
 builder.Services.AddScoped<ICreateDishService, CreateDishService>();
-builder.Services.AddScoped<IUpdateDishService, UpdateDishService>();
+builder.Services.AddScoped<IUpdateDishService, UpdateDishService  > ();
 builder.Services.AddScoped<IGetAllDishesService, GetAllDishesService>();
 builder.Services.AddScoped<IDishCommand, DishCommand>();
 builder.Services.AddScoped<IDishQuery, DishQuery>();
 
-var connectionString = builder.Configuration["ConnectionString"];
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
+
+// esto Aplica migraciones automáticamente al iniciar 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate(); 
+}
 
 if (app.Environment.IsDevelopment())
 {
@@ -57,6 +56,7 @@ if (app.Environment.IsDevelopment())
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "RestaurantAPI v1");
     });
 }
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
